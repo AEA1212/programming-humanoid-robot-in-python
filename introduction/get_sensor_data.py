4b736ae0276321f00fdc305b8272dab7c40819b3
Ayman <abdeldayem@campus.tu-berlin.de>
2024-10-27 20:20:25 +0100

    Introduction: get_sensor and set joint

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -10,6 +10,8 @@ In this exercise you need to know how to get sensor data.
 # add PYTHONPATH
 import os
 import sys
+
+
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
 
 from spark_agent import SparkAgent
@@ -21,6 +23,9 @@ class MyAgent(SparkAgent):
         temperature = 0
         # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
+        angle = perception.joint['HeadYaw']
+        temperature = perception.joint_temperature['HeadYaw']
+
 
         print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
         return super(MyAgent, self).think(perception)
